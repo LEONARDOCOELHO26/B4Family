24f72e3fc7879565ac882ff493e598c38e8b67d1
LEONARDOCOELHO26 <71470229+LEONARDOCOELHO26@users.noreply.github.com>
2023-01-23 22:04:57 -0300

    02

--- b4family/database/login_teste.py
+++ b4family/database/login_teste.py
@@ -3,6 +3,26 @@ from twilio.rest import Client
 import keys
 client = Client(keys.account_sid, keys.auth_token)
 
+import geocoder
+from geopy.geocoders import Nominatim
+g = geocoder.ip('me')
+
+from geopy.geocoders import Nominatim
+import datetime
+now = datetime.datetime.now()
+geolocator = Nominatim(user_agent="geoapiExercises")
+def city_state_country(coord):
+    location = geolocator.reverse(coord, exactly_one=True)
+    address = location.raw['address']
+    city = address.get('city', '')
+    state = address.get('state', '')
+    country = address.get('country', '')
+    return city, state, country
+print(city_state_country(g.latlng))
+time= print(now.day,"/",now.month,"/",now.year,now.hour,":",now.minute)
+
+
+
 import sqlite3 as sql
 conn  =  sql.connect ( 'bank_database.db' )
 cursor  =  conn.cursor ()
--- b4family/database/test.py
+++ b4family/database/test.py
@@ -2,7 +2,7 @@ import geocoder
 from geopy.geocoders import Nominatim
 g = geocoder.ip('me')
 
-from geopy.geocoders import Nominatim
+'''from geopy.geocoders import Nominatim
 import datetime
 now = datetime.datetime.now()
 geolocator = Nominatim(user_agent="geoapiExercises")
@@ -14,6 +14,22 @@ def city_state_country(coord):
     country = address.get('country', '')
     return city, state, country
 print(city_state_country(g.latlng))
-print(now.day,"/",now.month,"/",now.year,now.hour,":",now.minute)
+print(now.day,"/",now.month,"/",now.year,now.hour,":",now.minute)'''
+
+from marketing_notifications_python.models import app_db
+
+db = app_db()
+
+
+class Subscriber(db.Model):
+    __tablename__ = "subscribers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String, nullable=False)
+    subscribed = db.Column(db.Boolean, nullable=False, default=True)
+
+    def __repr__(self):
+        return '<Subscriber %r %r>' % self.phone_number, self.subscribed
+
 
 
